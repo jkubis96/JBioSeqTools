0fd12211f2da2a97b1b5b5dc3b4cc6267c1e1076
jkubis96 <jakub.kubis1996@gmail.com>
2022-12-15 13:50:17 +0100

    data_base_element

--- JBioSeqTools/__init__.py
+++ JBioSeqTools/__init__.py
@@ -6,6 +6,7 @@ print('Loading required  packages...')
 from JBioSeqTools.seq_tools import seq_tools
 from JBioSeqTools.vector_build import vector_build
 from JBioSeqTools.vector_graph import graph_plot
+from JBioSeqTools.api_elements import api
 
 
 print('JBioSeqTools is ready to use')
