1dd9711fc03da477d80aeb1ac9e9754f368bdc41
jkubis96 <jakub.kubis1996@gmail.com>
2025-08-28 15:53:14 +0200

    examples extending

--- examples.py
+++ examples.py
@@ -455,9 +455,147 @@ input_dict = {
 }
 
 
+#de novo - mRNA
+
+input_dict = {
+
+    # REQUIRED!
+    # name of current project (defined by user)
+    'project_name':'test_mRNA',
+    
+    # REQUIRED!
+    # avaiable options (human / mouse / rat / both (mouse + human) / both2 (rat + human) / multi (mouse + rat + human))
+    # 'both / both2 / multi' - creating vector function adjusted for all species taking into consideration most adjustments for Homo sapiens
+    'species':'human',
+    
+    # REQUIRED!
+    # string of coding sequence (CDS) provided to optimize sequence
+    # sequences orientation 5' ---> 3' - sense
+    'sequence':'ATGTTAACTAGTGGACTTGTAGTAAGCAACATGTTCTCCTATCATCTCGCAGCCTTGGGACTCATGCCGTCATTCCAGATGGAAGGGCGAGGTCGAGTTAATCAGCTAGGAGGTGTTTTCATTAATGGACGGCCACTGCCCAACCATATACGACTGAAGATTGTCGAGCTAGCGGCCCAGGGCGTCCGTCCGTGCGTCATCAGTAGACAGCTGCGGGTGTCACATGGCTGTGTCAGTAAAATACTCCAACGATATCAAGAAACCGGAAGTATCCGACCTGGGGTTATTGGCGGAAGTAAACCAAGGGTCGCAACTCCGGAAGTTGAGAAAAAGATAGAACAATACAAAAAAGATAATCCGGGAATTTTCAGTTGGGAGATTCGGGATCGGCTGCTGAAGGAGGGGATTTGTGACCGCAGCACCGTGCCAAGTGTGAGCTCCATCAGTCGAGTATTACGGAGCAGGTTCCAGAAATGTGATTCTGATGACAATGACAATGACAATGACAATGAGGACGACGATGGCGATGACGGCAGTAACAGTAGTGTGGCAGACAGGTCTGTTAACTTCTCTGTCAGCGGTCTGCTGTCCGACAATAAAAGCGACAAAAGCGACAACGATTCCGATTGTGAATCAGAGCCGGGGCTATCTGTAAAACGGAAGCAACGCCGCAGTCGAACTACTTTCACCGCGGAGCAGTTGGAGGAACTGGAAAGAGCCTTTGAACGAACTCACTATCCGGATATATATACGCGAGAGGAATTAGCACAAAGAACAAAGCTAACCGAGGCAAGAGTCCAAGTATGGTTTAGTAACCGAAGAGCGAGATGGCGGAAACAGATGGGTAGCAATCAGCTGACAGCCTTGAACAGTATATTACAAGTGCCACAGGGTATGGGAACGCCCTCTTATATGCTGCACGAGCCTGGGTATCCACTCTCACATAATGCAGACAATCTTTGGCATAGATCGTCTATGGCCCAGTCATTACAGTCATTTGGTCAGACAATAAAACCAGAGAATTCCTACGCCGGTCTTATGGAAAACTATTTATCTCATTCATCACAGCTTCATGGTCTTCCTACACATAGTTCATCCGATCCCCTCTCATCCACTTGGTCATCTCCCGTGTCCACTTCCGTTCCTGCGCTAGGATACACGCCATCTAGTGGCCATTACCATCATTACTCTGATGTCACCAAAAGTACTCTTCATTCATATAACGCTCATATTCCTTCAGTCACAAACATGGAGAGATGTTCAGTTGATGACAGTTTGGTTGCTTTACGTATGAAGTCACGTGAGCATTCCGCCGCTCTCAGTTTGATGCAGGTGGCAGACAACAAAATGGCTACCTCATTTTGA',
+    
+    # REQUIRED!
+    # name of coding sequence
+    'sequence_name':'SMN1',
+    
+    # OPTIONAL!
+    # restriction enzymes protection of transcript sequence
+    # if the user does not need any restriction places protection, provide empty list []
+    'restriction_list':['RsaI', 'MnlI', 'AciI', 'AluI', 'BmrI'],
+    
+    # REQUIRED!
+    # available options (True / False)
+    # decision; if the user wants the transcription sequence optimized based on the provided species
+    'optimize':True,
+    
+    # REQUIRED; if optimize == True!
+    # user-defined percent of GC% content in predicted/optimized sequence
+    'transcript_GC':58,
+    
+    # REQUIRED; if optimize == True!
+    # user-defined maximum number of consecutive repeats of a single nucleotide (A, C, T(U), G) in the predicted/optimized sequence, eg. AAAAAA
+    'poly_len':7
+    
+}
 
 
-# > example FAST for vector plot <
+#de novo - siRNA
+
+input_dict = {
+    
+    # REQUIRED!
+    # name of current project (defined by user)
+    'project_name':'test_siRNA',
+    
+    # REQUIRED!
+    # avaiable options (human / mouse / rat / both (mouse + human) / both2 (rat + human) / multi (mouse + rat + human))
+    # 'both / both2 / multi' - creating vector function adjusted for all species taking into consideration most adjustments for Homo sapiens
+    'species':'human',
+    
+    # REQUIRED!
+    # type of RNAi (defined by user)
+    # avaiable options 'sh' - for shRNA and 'sirna' for siRNA
+    'rnai_type':'sirna',
+    
+    # OPTIONAL!
+    # sequence of custom RNAi, which can be provided by user
+    # if provided, then the algorithm of RNAi estimation is off
+    # if empt '' the algorithm share the best possible RNAi based on 'rnai_gene_name'
+    # sequence orientation 5' ---> 3' - sense
+    'rnai_sequence':'',
+    
+    # REQUIRED; if rnai_sequence is empty!
+    # length of RNAi (defined by user)
+    # recomended range: 20–25
+    'rnai_length':20,
+    
+    # OPTIONAL; if rnai_sequence is empty!
+    # overhang sequence of RNAi sequence 3' end (defined by user)
+    # recomended 'UU'
+    'overhang_3_prime':'UU',
+    
+    # REQUIRED!
+    # name of the target gene for the RNAi searching algorithm (gene name for Homo sapien / Mus musculus / Rattus norvegicus)
+    # algorithm is working when the rnai_sequence is empty ''
+    # if the user defines 'rnai_sequence' this 'rnai_gene_name' is just a name for a user-supplied sequence
+    'rnai_gene_name':'KIT',
+    
+    # REQUIRED, if rnai_type = 'sh'!
+    # sequence of the loop to create the structure of the hairpin of shRNA 
+    # sequence orientation 5' ---> 3' - sense
+    'loop_sequence':''
+}
+
+
+#de novo - shRNA
+
+input_dict = {
+
+    # REQUIRED!
+    # name of current project (defined by user)
+    'project_name':'test_shRNA',
+    
+    # REQUIRED!
+    # avaiable options (human / mouse / rat / both (mouse + human) / both2 (rat + human) / multi (mouse + rat + human))
+    # 'both / both2 / multi' - creating vector function adjusted for all species taking into consideration most adjustments for Homo sapiens
+    'species':'human',
+    
+    # REQUIRED!
+    # type of RNAi (defined by user)
+    # avaiable options 'sh' - for shRNA and 'sirna' for siRNA
+    'rnai_type':'sh',
+    
+    # OPTIONAL!
+    # sequence of custom RNAi, which can be provided by user
+    # if provided, then the algorithm of RNAi estimation is off
+    # if empt '' the algorithm share the best possible RNAi based on 'rnai_gene_name'
+    # sequence orientation 5' ---> 3' - sense
+    'rnai_sequence':'',
+    
+    # REQUIRED; if rnai_sequence is empty!
+    # length of RNAi (defined by user)
+    # recomended range: 20–25
+    'rnai_length':20,
+    
+    # OPTIONAL; if rnai_sequence is empty!
+    # overhang sequence of RNAi sequence 3' end (defined by user)
+    # recomended 'UU'
+    'overhang_3_prime':'UU',
+    
+    # REQUIRED!
+    # name of the target gene for the RNAi searching algorithm (gene name for Homo sapien / Mus musculus / Rattus norvegicus)
+    # algorithm is working when the rnai_sequence is empty ''
+    # if the user defines 'rnai_sequence' this 'rnai_gene_name' is just a name for a user-supplied sequence
+    'rnai_gene_name':'KIT',
+    
+    # REQUIRED, if rnai_type = 'sh'!
+    # sequence of the loop to create the structure of the hairpin of shRNA 
+    # sequence orientation 5' ---> 3' - sense
+    'loop_sequence':'TAGTGAAGCCACAGATGTAC'
+}
+
+
+
+# > example FASTA for vector plot <
 
 # # test_expression_ssAAV_expression_8717nc
 
@@ -505,4 +643,185 @@ input_dict = {
 # AACGCCAGCAACGCGGCCTTTTTACGGTTCCTGGCCTTTTGCTGGCCTTTTGCTCACATGTCCTGCAGGCAG
 
 
+
+# > example GeneBank for vector notation <
+
+# LOCUS       viral_vector   8717 bp    DNA     circular     11-JUL-2025
+# DEFINITION   Synthetic viral plasmid vector.
+# FEATURES             Location/Qualifiers
+#      misc_feature    1..130
+#                      /note="5`ITR"
+#      misc_feature    158..617
+#                      /note="Promoter:TBG"
+#      misc_feature    642..647
+#                      /note="Kozak_sequence"
+#      misc_feature    648..1532
+#                      /note="SEQ1:SMN1"
+#      misc_feature    1533..2420
+#                      /note="SEQ2:SMN2"
+#      misc_feature    2421..2642
+#                      /note="PolyA_signal:SV40"
+#      misc_feature    2643..4375
+#                      /note="2nd_promoter:CAG"
+#      misc_feature    4376..5095
+#                      /note="Fluorescent_tag:EGFP"
+#      misc_feature    5126..5723
+#                      /note="Enhancer:WPRE"
+#      misc_feature    5754..5961
+#                      /note="2nd_polyA_signal:bGH"
+#      misc_feature    5969..6098
+#                      /note="3`ITR"
+#      misc_feature    7026..7886
+#                      /note="Resistance:Ampicillin"
+#      misc_feature    8057..8645
+#                      /note="pUC_ori"
+
+# ORIGIN
+#         1 ctgcgcgctc gctcgctcac tgaggccgcc cgggcaaagc ccgggcgtcg ggcgaccttt
+#        61 ggtcgcccgg cctcagtgag cgagcgagcg cgcagagagg gagtggccaa ctccatcact
+#       121 aggggttcct tctagacaac tttgtataga aaagttgggg ctggaagcta cctttgacat
+#       181 catttcctct gcgaatgcat gtataatttc tacagaacct attagaaagg atcacccagc
+#       241 ctctgctttt gtacaacttt cccttaaaaa actgccaatt ccactgctgt ttggcccaat
+#       301 agtgagaact ttttcctgct gcctcttggt gcttttgcct atggccccta ttctgcctgc
+#       361 tgaagacact cttgccagca tggacttaaa cccctccagc tctgacaatc ctctttctct
+#       421 tttgttttac atgaagggtc tggcagccaa agcaatcact caaagttcaa accttatcat
+#       481 tttttgcttt gttcctcttg gccttggttt tgtacatcag ctttgaaaat accatcccag
+#       541 ggttaatgct ggggttaatt tataactaag agtgctctag ttttgcaata caggacatgc
+#       601 tataaaaatg gaaagatcaa gtttgtacaa aaaagcaggc tgccaccatg gctatgtcta
+#       661 gcggaggctc tggaggagga gttcctgaac aggaggactc tgtgctgttc cggaggggca
+#       721 caggacaaag cgatgacagc gacatctggg acgacacagc tctgattaag gcctacgaca
+#       781 aggccgtggc cagcttcaag cacgccctga agaacggcga catctgcgag accagcggaa
+#       841 agcctaaaac cacccctaag agaaagcctg ctaaaaagaa caagagccag aagaagaaca
+#       901 ccgctgccag cctgcagcag tggaaggtgg gcgacaagtg cagcgccatt tggagcgagg
+#       961 acggatgtat ctaccctgcc acaatcgcca gcatcgactt caagcgggag acctgcgtgg
+#      1021 tggtgtatac cggctacggc aacagggaag agcagaacct gagcgacctg ctgagcccta
+#      1081 tttgcgaggt ggccaataac atcgagcaga acgcccagga gaacgagaac gagagccagg
+#      1141 tgagcaccga cgagagcgag aacagccgga gccccggcaa taagagcgac aacatcaagc
+#      1201 ccaagagcgc cccctggaac tctttcctgc cccccccccc ccccatgcct ggacctagat
+#      1261 tgggacctgg aaaacctgga ctgaaattca acggcccccc cccccccccc cccccccccc
+#      1321 ccccccattt gctgtcttgt tggctgcccc ccttcccttc tggacccccc attatccccc
+#      1381 cccccccccc catctgtcct gattctctgg acgacgccga tgctttgggc tctatgctga
+#      1441 tctcttggta tatgagcggc taccacaccg gctactacat gttccccgag gccagcctga
+#      1501 aggccgagca gatgcccgct ccttgttttc tgatggctat gtctagcgga ggctctggag
+#      1561 gaggagttcc tgaacaggag gactctgtgc tgttccggag gggcacagga caaagcgatg
+#      1621 acagcgacat ctgggacgac acagctctga ttaaggccta cgacaaggcc gtggccagct
+#      1681 tcaagcacgc cctgaagaac ggcgacatct gcgagaccag cggaaagcct aaaaccaccc
+#      1741 ctaagagaaa gcctgctaaa aagaacaaga gccagaagaa gaacaccgct gccagcctgc
+#      1801 agcagtggaa ggtgggcgac aagtgcagcg ccatttggag cgaggacgga tgtatctacc
+#      1861 ctgccacaat cgccagcatc gacttcaagc gggagacctg cgtggtggtg tataccggct
+#      1921 acggcaacag ggaagagcag aacctgagcg acctgctgag ccctatttgc gaggtggcca
+#      1981 ataacatcga gcagaacgcc caggagaacg agaacgagag ccaggtgagc accgacgaga
+#      2041 gcgagaacag ccggagcccc ggcaataaga gcgacaacat caagcccaag agcgccccct
+#      2101 ggaactcttt cctgcccccc ccccccccca tgcctggacc tagattggga cctggaaaac
+#      2161 ctggactgaa attcaacggc cccccccccc cccccccccc cccccccccc catttgctgt
+#      2221 cttgttggct gccccccttc ccttctggac cccccattat cccccccccc ccccccatct
+#      2281 gtcctgattc tctggacgac gccgatgctt tgggctctat gctgatctct tggtatatga
+#      2341 gcggctacca caccggctac tacatgttcc ccgaggccag cctgaaggcc gagcagatgc
+#      2401 ccgctccttg ttttctgtga cagacatgat aagatacatt gatgagtttg gacaaaccac
+#      2461 aactagaatg cagtgaaaaa aatgctttat ttgtgaaatt tgtgatgcta ttgctttatt
+#      2521 tgtaaccatt ataagctgca ataaacaagt taacaacaac aattgcattc attttatgtt
+#      2581 tcaggttcag ggggaggtgt gggaggtttt ttaaagcaag taaaacctct acaaatgtgg
+#      2641 tactcgacat tgattattga ctagttatta atagtaatca attacggggt cattagttca
+#      2701 tagcccatat atggagttcc gcgttacata acttacggta aatggcccgc ctggctgacc
+#      2761 gcccaacgac ccccgcccat tgacgtcaat aatgacgtat gttcccatag taacgccaat
+#      2821 agggactttc cattgacgtc aatgggtgga gtatttacgg taaactgccc acttggcagt
+#      2881 acatcaagtg tatcatatgc caagtacgcc ccctattgac gtcaatgacg gtaaatggcc
+#      2941 cgcctggcat tatgcccagt acatgacctt atgggacttt cctacttggc agtacatcta
+#      3001 cgtattagtc atcgctatta ccatggtcga ggtgagcccc acgttctgct tcactctccc
+#      3061 catctccccc ccctccccac ccccaatttt gtatttattt attttttaat tattttgtgc
+#      3121 agcgatgggg gcgggggggg ggggggggcg cgcgccaggc ggggcggggc ggggcgaggg
+#      3181 gcggggcggg gcgaggcgga gaggtgcggc ggcagccaat cagagcggcg cgctccgaaa
+#      3241 gtttcctttt atggcgaggc ggcggcggcg gcggccctat aaaaagcgaa gcgcgcggcg
+#      3301 ggcgggagtc gctgcgcgct gccttcgccc cgtgccccgc tccgccgccg cctcgcgccg
+#      3361 cccgccccgg ctctgactga ccgcgttact cccacaggtg agcgggcggg acggcccttc
+#      3421 tcctccgggc tgtaattagc gcttggttta atgacggctt gtttcttttc tgtggctgcg
+#      3481 tgaaagcctt gaggggctcc gggagggccc tttgtgcggg gggagcggct cggggggtgc
+#      3541 gtgcgtgtgt gtgtgcgtgg ggagcgccgc gtgcggctcc gcgctgcccg gcggctgtga
+#      3601 gcgctgcggg cgcggcgcgg ggctttgtgc gctccgcagt gtgcgcgagg ggagcgcggc
+#      3661 cgggggcggt gccccgcggt gcgggggggg ctgcgagggg aacaaaggct gcgtgcgggg
+#      3721 tgtgtgcgtg ggggggtgag cagggggtgt gggcgcgtcg gtcgggctgc aaccccccct
+#      3781 gcacccccct ccccgagttg ctgagcacgg cccggcttcg ggtgcggggc tccgtacggg
+#      3841 gcgtggcgcg gggctcgccg tgccgggcgg ggggtggcgg caggtggggg tgccgggcgg
+#      3901 ggcggggccg cctcgggccg gggagggctc gggggagggg cgcggcggcc cccggagcgc
+#      3961 cggcggctgt cgaggcgcgg cgagccgcag ccattgcctt ttatggtaat cgtgcgagag
+#      4021 ggcgcaggga cttcctttgt cccaaatctg tgcggagccg aaatctggga ggcgccgccg
+#      4081 caccccctct agcgggcgcg gggcgaagcg gtgcggcgcc ggcaggaagg aaatgggcgg
+#      4141 ggagggcctt cgtgcgtcgc cgcgccgccg tccccttctc cctctccagc ctcggggctg
+#      4201 tccgcggggg gacggctgcc ttcggggggg acggggcagg gcggggttcg gcttctggcg
+#      4261 tgtgaccggc ggctctagag cctctgctaa ccatgttcat gccttcttct ttttcctaca
+#      4321 gctcctgggc aacgtgctgg ttattgtgct gtctcatcat tttggcaaag aattgatggt
+#      4381 gagcaagggc gaggagctgt tcaccggggt ggtgcccatc ctggtcgagc tggacggcga
+#      4441 cgtaaacggc cacaagttca gcgtgtccgg cgagggcgag ggcgatgcca cctacggcaa
+#      4501 gctgaccctg aagttcatct gcaccaccgg caagctgccc gtgccctggc ccaccctcgt
+#      4561 gaccaccctg acctacggcg tgcagtgctt cagccgctac cccgaccaca tgaagcagca
+#      4621 cgacttcttc aagtccgcca tgcccgaagg ctacgtccag gagcgcacca tcttcttcaa
+#      4681 ggacgacggc aactacaaga cccgcgccga ggtgaagttc gagggcgaca ccctggtgaa
+#      4741 ccgcatcgag ctgaagggca tcgacttcaa ggaggacggc aacatcctgg ggcacaagct
+#      4801 ggagtacaac tacaacagcc acaacgtcta tatcatggcc gacaagcaga agaacggcat
+#      4861 caaggtgaac ttcaagatcc gccacaacat cgaggacggc agcgtgcagc tcgccgacca
+#      4921 ctaccagcag aacaccccca tcggcgacgg ccccgtgctg ctgcccgaca accactacct
+#      4981 gagcacccag tccgccctga gcaaagaccc caacgagaag cgcgatcaca tggtcctgct
+#      5041 ggagttcgtg accgccgccg ggatcactct cggcatggac gagctgtaca agtaaaccca
+#      5101 gctttcttgt acaaagtggg aattccgata atcaacctct ggattacaaa atttgtgaaa
+#      5161 gattgactgg tattcttaac tatgttgctc cttttacgct atgtggatac gctgctttaa
+#      5221 tgcctttgta tcatgctatt gcttcccgta tggctttcat tttctcctcc ttgtataaat
+#      5281 cctggttgct gtctctttat gaggagttgt ggcccgttgt caggcaacgt ggcgtggtgt
+#      5341 gcactgtgtt tgctgacgca acccccactg gttggggcat tgccaccacc tgtcagctcc
+#      5401 tttccgggac tttcgctttc cccctcccta ttgccacggc ggaactcatc gccgcctgcc
+#      5461 ttgcccgctg ctggacaggg gctcggctgt tgggcactga caattccgtg gtgttgtcgg
+#      5521 ggaagctgac gtcctttcca tggctgctcg cctgtgttgc cacctggatt ctgcgcggga
+#      5581 cgtccttctg ctacgtccct tcggccctca atccagcgga ccttccttcc cgcggcctgc
+#      5641 tgccggctct gcggcctctt ccgcgtcttc gccttcgccc tcagacgagt cggatctccc
+#      5701 tttgggccgc ctccccgcat cgggaattcc tagagctcgc tgatcagcct cgactgtgcc
+#      5761 ttctagttgc cagccatctg ttgtttgccc ctcccccgtg ccttccttga ccctggaagg
+#      5821 tgccactccc actgtccttt cctaataaaa tgaggaaatt gcatcgcatt gtctgagtag
+#      5881 gtgtcattct attctggggg gtggggtggg gcaggacagc aagggggagg attgggaaga
+#      5941 gaatagcagg catgctgggg agggccgcct gcgcgctcgc tcgctcactg aggccgcccg
+#      6001 ggcaaagccc gggcgtcggg cgacctttgg tcgcccggcc tcagtgagcg agcgagcgcg
+#      6061 cagagaggga gtggccaact ccatcactag gggttcctct gcctgcaggg gcgcctgatg
+#      6121 cggtattttc tccttacgca tctgtgcggt atttcacacc gcatacgtca aagcaaccat
+#      6181 agtacgcgcc ctgtagcggc gcattaagcg cggcgggggt ggtggttacg cgcagcgtga
+#      6241 ccgctacact tgccagcgcc ttagcgcccg ctcctttcgc tttcttccct tcctttctcg
+#      6301 ccacgttcgc cggctttccc cgtcaagctc taaatcgggg gctcccttta gggttccgat
+#      6361 ttagtgcttt acggcacctc gaccccaaaa aacttgattt gggtgatggt tcacgtagtg
+#      6421 ggccatcgcc ctgatagacg gtttttcgcc ctttgacgtt ggagtccacg ttctttaata
+#      6481 gtggactctt gttccaaact ggaacaacac tcaactctat ctcgggctat tcttttgatt
+#      6541 tataagggat tttgccgatt tcggtctatt ggttaaaaaa tgagctgatt taacaaaaat
+#      6601 ttaacgcgaa ttttaacaaa atattaacgt ttacaatttt atggtgcact ctcagtacaa
+#      6661 tctgctctga tgccgcatag ttaagccagc cccgacaccc gccaacaccc gctgacgcgc
+#      6721 cctgacgggc ttgtctgctc ccggcatccg cttacagaca agctgtgacc gtctccggga
+#      6781 gctgcatgtg tcagaggttt tcaccgtcat caccgaaacg cgcgagacga aagggcctcg
+#      6841 tgatacgcct atttttatag gttaatgtca tgataataat ggtttcttag acgtcaggtg
+#      6901 gcacttttcg gggaaatgtg cgcggaaccc ctatttgttt atttttctaa atacattcaa
+#      6961 atatgtatcc gctcatgaga caataaccct gataaatgct tcaataatat tgaaaaagga
+#      7021 agagtatgag tattcaacat ttccgtgtcg cccttattcc cttttttgcg gcattttgcc
+#      7081 ttcctgtttt tgctcaccca gaaacgctgg tgaaagtaaa agatgctgaa gatcagttgg
+#      7141 gtgcacgagt gggttacatc gaactggatc tcaacagcgg taagatcctt gagagttttc
+#      7201 gccccgaaga acgttttcca atgatgagca cttttaaagt tctgctatgt ggcgcggtat
+#      7261 tatcccgtat tgacgccggg caagagcaac tcggtcgccg catacactat tctcagaatg
+#      7321 acttggttga gtactcacca gtcacagaaa agcatcttac ggatggcatg acagtaagag
+#      7381 aattatgcag tgctgccata accatgagtg ataacactgc ggccaactta cttctgacaa
+#      7441 cgatcggagg accgaaggag ctaaccgctt ttttgcacaa catgggggat catgtaactc
+#      7501 gccttgatcg ttgggaaccg gagctgaatg aagccatacc aaacgacgag cgtgacacca
+#      7561 cgatgcctgt agcaatggca acaacgttgc gcaaactatt aactggcgaa ctacttactc
+#      7621 tagcttcccg gcaacaatta atagactgga tggaggcgga taaagttgca ggaccacttc
+#      7681 tgcgctcggc ccttccggct ggctggttta ttgctgataa atctggagcc ggtgagcgtg
+#      7741 gaagccgcgg tatcattgca gcactggggc cagatggtaa gccctcccgt atcgtagtta
+#      7801 tctacacgac ggggagtcag gcaactatgg atgaacgaaa tagacagatc gctgagatag
+#      7861 gtgcctcact gattaagcat tggtaactgt cagaccaagt ttactcatat atactttaga
+#      7921 ttgatttaaa acttcatttt taatttaaaa ggatctaggt gaagatcctt tttgataatc
+#      7981 tcatgaccaa aatcccttaa cgtgagtttt cgttccactg agcgtcagac cccgtagaaa
+#      8041 agatcaaagg atcttcttga gatccttttt ttctgcgcgt aatctgctgc ttgcaaacaa
+#      8101 aaaaaccacc gctaccagcg gtggtttgtt tgccggatca agagctacca actctttttc
+#      8161 cgaaggtaac tggcttcagc agagcgcaga taccaaatac tgttcttcta gtgtagccgt
+#      8221 agttaggcca ccacttcaag aactctgtag caccgcctac atacctcgct ctgctaatcc
+#      8281 tgttaccagt ggctgctgcc agtggcgata agtcgtgtct taccgggttg gactcaagac
+#      8341 gatagttacc ggataaggcg cagcggtcgg gctgaacggg gggttcgtgc acacagccca
+#      8401 gcttggagcg aacgacctac accgaactga gatacctaca gcgtgagcta tgagaaagcg
+#      8461 ccacgcttcc cgaagggaga aaggcggaca ggtatccggt aagcggcagg gtcggaacag
+#      8521 gagagcgcac gagggagctt ccagggggaa acgcctggta tctttatagt cctgtcgggt
+#      8581 ttcgccacct ctgacttgag cgtcgatttt tgtgatgctc gtcagggggg cggagcctat
+#      8641 ggaaaaacgc cagcaacgcg gcctttttac ggttcctggc cttttgctgg ccttttgctc
+#      8701 acatgtcctg caggcag
+# //
 
