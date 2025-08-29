109bb7912fa8da3f772d05ec09911fe716fcb633
jkubis96 <jakub.kubis1996@gmail.com>
2025-08-29 10:11:04 +0200

    test

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 VERSION = '2.1.0' 
-DESCRIPTION = 'JBioSeqTools'
+DESCRIPTION = 'jbioseqtools'
 LONG_DESCRIPTION = Path("README.md").read_text(encoding="utf-8")
 
 # Setting up
 setup(
-        name="JBioSeqTools", 
+        name="jbioseqtools", 
         version=VERSION,
         author="Jakub Kubis",
         author_email="jbiosystem@gmail.com",
@@ -16,7 +16,7 @@ setup(
         packages=['jbst'],
         include_package_data=True,
         install_requires=['pandas', 'tqdm', 'matplotlib', 'numpy', 'requests', 'openpyxl', 'pymsaviz==0.4.2', 'ViennaRNA==2.6.4', 'biopython==1.81', 'networkx==3.1', 'seaborn', 'scipy', 'gdown==5.2.0'],       
-        keywords=['sequence', 'optimization', 'vectors', 'AAV', 'GC', 'restriction enzyme'],
+        keywords=['sequence', 'optimization', 'vectors', 'AAV', 'GC', 'restriction enzyme', 'therapies', 'design','genetic'],
         license = 'MIT',
         classifiers = [
             "Development Status :: 3 - Alpha",
